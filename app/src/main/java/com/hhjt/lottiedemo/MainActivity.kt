95b567703fa7fda82a0e027c2217eb33c6b2cb96
zuriel <13147106733@163.com>
2019-04-25 14:20:53 +0800

    wx

--- app/src/main/java/com/hhjt/lottiedemo/MainActivity.kt
+++ app/src/main/java/com/hhjt/lottiedemo/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("222")
+        print("444")
 
 //        lottie.imageAssetsFolder = "images/"
 //        lottie.setAnimationFromJson("qd1.json")
